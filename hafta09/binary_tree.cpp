7644264b4fe68a685eaa6ccd054e05df6b164c1d
GBayram <bayramgokmenn@gmail.com>
2018-12-17 13:47:10 +0300

    c
    c

---
+++ hafta09/binary_tree.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// basic binary tree
+struct Node {
+
+	int data; // veriyi tutacagimiz yapi.
+	Node *left; // solu gosterecegimiz pointer.
+	Node *right; // sagi gosterecegimiz pointer.
+};
+
+typedef struct Node Node;
+
+struct Node* newNode(int data) {
+
+	Node *n = (Node*)malloc(sizeof(Node));
+	n->data = data; // gelen data n dugumumuzun datasina esit olsun.
+	n->left = n->right = NULL;
+}
+
+int main() {
+	Node *root = newNode(1); // kok dugumumuzu olusturduk.
+	root->left = newNode(3); // kok dugumuzun soluna 3 degerini koyduk.
+	root->right = newNode(4); // kok dugumuzun sagina 4 degerini koyduk.
+	root->left->left = newNode(5); 
+}
